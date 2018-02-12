f3e8863d9694260a00c10e15afbe29606e0da69c
Jonathan Sax <jsax8310@outlook.com>
2018-02-12 15:28:43 -0700

    styled the login window

--- src/components/home/Login.vue
+++ src/components/home/Login.vue
@@ -4,8 +4,12 @@
      <b-card>
        <b-container>
         <b-row class="my-1" v-for="type in types" :key="type">
-          <b-col sm="3"><label :for="`type-${type}`">{{ type }}:</label></b-col>
-          <b-col sm="9"><b-form-input :id="`type-${type}`" :type="type"></b-form-input></b-col>
+          <b-col sm="3" class='labels'><label :for="`type-${type}`">{{ type }}:</label></b-col>
+          <b-col sm="6"><b-form-input :id="`type-${type}`" :type="type"></b-form-input></b-col>
+        </b-row>
+        <b-row>
+          <b-col sm='1' class='loginButton'><b-button>Login</b-button></b-col>
+          <b-col sm='3' class='signupButton'><b-button>Sign Up</b-button></b-col>
         </b-row>
        </b-container>
      </b-card>
@@ -30,5 +34,11 @@ export default {
 </script>
 
 <style>
+.labels {
+  margin-left: 15%;
+}
+.loginButton {
+  margin-left: 35%;
+}
 
 </style>
